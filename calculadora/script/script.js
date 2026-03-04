e76156ec76c4ab3ad5ddd0d4bddf1df15e14e1e4
DevGustavoJeferson <gustavojeferson2024@gmail.com>
2026-03-04 13:04:51 -0300

    update: script.js

--- calculadora/script/script.js
+++ calculadora/script/script.js
@@ -1,8 +1,56 @@
-//PEGANDO O VALOR DO DISPLAY
-let display = document.querySelector('.display')
+//PEGANDO OS VALORES DO HTML
+const display = document.querySelector('.display')
+const botoes = document.querySelectorAll('button')
 
-//FUNCAO PARA OPERACOES
-function operation(button){
-    var valor = button.dataset.value;
-    display.innerText = valor
-}
+botoes.forEach((botao) => {
+    botao.addEventListener('click', () => {
+        const valor = botao.dataset.value;
+        switch (valor) {
+            case '1':
+                display.textContent += `${valor}`
+                break;
+            case '2':
+                display.textContent += `${valor}`
+                break;
+            case '3':
+                display.textContent += `${valor}`
+                break;
+            case '4':
+                display.textContent += `${valor}`
+                break;
+            case '5':
+                display.textContent += `${valor}`
+                break;
+            case '6':
+                display.textContent += `${valor}`
+                break;
+            case '7':
+                display.textContent += `${valor}`
+                break;
+            case '8':
+                display.textContent += `${valor}`
+                break;
+            case '9':
+                display.textContent += `${valor}`
+                break;
+            case 'C':
+                display.textContent += `Limpar tudo`
+                break;
+            case '+':
+                display.textContent += `${valor}`
+                break;
+            case '-':
+                display.textContent += `${valor}`
+                break;
+            case '*':
+                display.textContent += `${valor}`
+                break;
+            case '/':
+                display.textContent += `${valor}`
+                break;
+            case '=':
+                display.textContent += `Realizar operação`
+                break;
+        }
+    })
+});
